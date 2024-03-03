c0e488437ba45e2ffb5630b0d09440c27dc52f33
Luis Fellipe Batista Silva <63753269+offellipe@users.noreply.github.com>
2024-03-03 09:54:35 -0300

    numpy_handler - Aula 14

--- src/drivers/numpy_handler.py
+++ src/drivers/numpy_handler.py
@@ -1,8 +1,9 @@
 import numpy
 from typing import List
+from .interfaces.driver_handler_interface import DriverHandlerInterface
 
 
-class NumpyHandler:
+class NumpyHandler(DriverHandlerInterface):
     def __init__(self) -> None:
         self.__np = numpy
 
