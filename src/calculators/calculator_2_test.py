21621c973fa4d660bc04b886b15263ad646427e0
Luis Fellipe Batista Silva <63753269+offellipe@users.noreply.github.com>
2024-03-03 10:23:14 -0300

    calculator_2 - Aula 15

--- src/calculators/calculator_2_test.py
+++ src/calculators/calculator_2_test.py
@@ -1,6 +1,9 @@
+from typing import Dict, List
 from .calculator_2 import Calculator2
 from src.drivers.numpy_handler import NumpyHandler
-from typing import Dict
+from src.drivers.interfaces.driver_handler_interface import (
+    DriverHandlerInterface
+)
 
 
 class MockRequest:
@@ -8,7 +11,12 @@ class MockRequest:
         self.json = body
 
 
-def test_calculate():
+class MockDriverHandler(DriverHandlerInterface):
+    def standard_derivation(self, number: List[float]) -> float:
+        return 3
+
+
+def test_calculate_integration():
     mock_request = MockRequest({
         "numbers": [2.12, 4.62, 1.32]
         })  # type: ignore
@@ -19,3 +27,16 @@ def test_calculate():
 
     assert isinstance(formated_response, dict)
     assert formated_response == {'data': {'Calculator': 2, 'result': 0.08}}
+
+
+def test_calculate():
+    mock_request = MockRequest({
+        "numbers": [2.12, 4.62, 1.32]
+        })  # type: ignore
+
+    driver = MockDriverHandler()
+    calculator_2 = Calculator2(driver)
+    formated_response = calculator_2.calculate(mock_request)
+
+    assert isinstance(formated_response, dict)
+    assert formated_response == {'data': {'Calculator': 2, 'result': 0.33}}
