c2a877b86e0a95bdb37b859eb1f9573807b3fb9f
Luis Fellipe Batista Silva <63753269+offellipe@users.noreply.github.com>
2024-03-02 23:15:45 -0300

    calculators - Aula 06

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -7,8 +7,6 @@ calc_route_bp = Blueprint("calc_routes", __name__)
 @calc_route_bp.route("/calculator/1", methods=["POST"])
 def calculator1():
     calc = Calculator1()
-    calc.calculate(request)
+    response = calc.calculate(request)
 
-    return jsonify({
-        "success": True,
-        }), 200
+    return jsonify(response), 200
