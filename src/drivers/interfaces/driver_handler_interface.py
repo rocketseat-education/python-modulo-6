780a38386023a27d2f2130901fa627c467299058
Luis Fellipe Batista Silva <63753269+offellipe@users.noreply.github.com>
2024-03-03 09:53:41 -0300

    Add files via upload

---
+++ src/drivers/interfaces/driver_handler_interface.py
@@ -0,0 +1,9 @@
+from abc import ABC, abstractclassmethod
+from typing import List
+
+
+class DriverHandlerInterface(ABC):
+
+    @abstractclassmethod
+    def standard_derivation(self, number: List[float]) -> float:
+        pass
