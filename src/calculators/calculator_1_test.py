088bdb6e5f1cceab3dd43159c4877ad70adf33b9
Luis Fellipe Batista Silva <63753269+offellipe@users.noreply.github.com>
2024-03-03 07:29:12 -0300

    calculator_1_test - Aula 09

--- src/calculators/calculator_1_test.py
+++ src/calculators/calculator_1_test.py
@@ -1,5 +1,6 @@
 from .calculator_1 import Calculator1
 from typing import Dict
+from pytest import raises
 
 
 class MockRequest:
@@ -19,3 +20,13 @@ def test_calculate():
 
     assert response["data"]["result"] == 14.25
     assert response["data"]["Calculator"] == 1
+
+
+def test_calculate_with_body_error():
+    mock_request = MockRequest(body={"something": 1})
+    calculator_1 = Calculator1
+
+    with raises(Exception) as excinfo:
+        calculator_1.calculate(mock_request)
+
+    assert str(excinfo.value) == "body mal formatado!"
