c6532150173b5131a542b22dcf199a5692cb66d9
Luis Fellipe Batista Silva <63753269+offellipe@users.noreply.github.com>
2024-03-02 23:13:58 -0300

    calculator_1 - Aula 06

--- src/calculators/calculator_1.py
+++ src/calculators/calculator_1.py
@@ -4,13 +4,19 @@ from flask import request as FlaskRequest
 
 class Calculator1:
 
-    def calculate(self, request: FlaskRequest) -> Dict: # type: ignore
+    def calculate(self, request: FlaskRequest) -> Dict:  # type: ignore
         body = request.json
         input_data = self.__validate_body(body)
         splited_number = input_data / 3
 
         first_process_result = self.__first_process(splited_number)
+        second_process_result = self.__second_process(splited_number)
+        calc_result = first_process_result + second_process_result + \
+            splited_number
 
+        response = self.__format_response(calc_result)
+
+        return response
 
     def __validate_body(self, body: Dict) -> float:
         if "number" not in body:
@@ -23,3 +29,16 @@ class Calculator1:
         first_part = (first_number / 4) + 7
         second_part = (first_part ** 2) * 0.257
         return second_part
+
+    def __second_process(self, second_number: float) -> float:
+        first_part = (second_number ** 2.121)
+        second_part = (first_part / 5) + 1
+        return second_part
+
+    def __format_response(self, calc_result: float) -> Dict:
+        return {
+            "data": {
+                "Calculator": 1,
+                "result": round(calc_result, 2)
+            }
+        }
