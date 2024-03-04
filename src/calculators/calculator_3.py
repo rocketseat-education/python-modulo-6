baad6b50bb5ebcc27fc4bbcbf1c6ce1091df94c2
Luis Fellipe Batista Silva <63753269+offellipe@users.noreply.github.com>
2024-03-04 11:22:12 -0300

    calculator_3 - Aula 20

--- src/calculators/calculator_3.py
+++ src/calculators/calculator_3.py
@@ -16,7 +16,7 @@ class Calculator3:
         variance = self.__calculate_variance(input_data)
         multiplication = self.__calculate_multiplication(input_data)
         self.__verify_results(variance, multiplication)
-        formated_response = self.__format_response(multiplication)
+        formated_response = self.__format_response(variance)
 
         return formated_response
 
