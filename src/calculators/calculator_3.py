ab86f010f6c8d311dcb3f25cbc896b0fa02a0284
Luis Fellipe Batista Silva <63753269+offellipe@users.noreply.github.com>
2024-03-04 10:50:05 -0300

    calculator_3 - Aula 19

--- src/calculators/calculator_3.py
+++ src/calculators/calculator_3.py
@@ -1,5 +1,5 @@
 from flask import request as FlaskRequest
-from typing import Dict
+from typing import Dict, List
 from src.drivers.interfaces.driver_handler_interface import (
     DriverHandlerInterface
 )
@@ -10,4 +10,44 @@ class Calculator3:
         self.__driver_handler = driver_handler
 
     def calculate(self, request: FlaskRequest) -> Dict:  # type: ignore
-        pass
+        body = request.json
+        input_data = self.__validate_body(body)
+
+        variance = self.__calculate_variance(input_data)
+        multiplication = self.__calculate_multiplication(input_data)
+        self.__verify_results(variance, multiplication)
+        formated_response = self.__format_response(multiplication)
+
+        return formated_response
+
+    def __validate_body(self, body: Dict) -> List[float]:
+        if "numbers" not in body:
+            raise Exception("body mal formatado!")
+
+        input_data = body["numbers"]
+        return input_data
+
+    def __calculate_variance(self, numbers: List[float]) -> float:
+        variance = self.__driver_handler.variance(numbers)
+        return variance
+
+    def __calculate_multiplication(self, numbers: List[float]) -> float:
+        multiplication = 1
+        for num in numbers:
+            multiplication *= num
+
+        return multiplication
+
+    def __verify_results(self, variance: float, multiplication: float) -> None:
+        if variance < multiplication:
+            raise Exception('Falha no processo: Variância menor que \
+                            multiplicação')
+
+    def __format_response(self, variance: float) -> Dict:
+        return {
+            "data": {
+                "Calculator": 3,
+                "value": variance,
+                "Success": True,
+            }
+        }
