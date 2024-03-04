5f62c77a0b1e5082631df97677cda8246c1e0916
Luis Fellipe Batista Silva <63753269+offellipe@users.noreply.github.com>
2024-03-04 11:39:59 -0300

    calculators - Aula 21

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify, request
+from src.main.factories.calculator3_factory import calculator3_factory
 from src.main.factories.calculator2_factory import calculator2_factory
 from src.main.factories.calculator1_factory import calculator1_factory
 
@@ -7,7 +8,7 @@ calc_route_bp = Blueprint("calc_routes", __name__)
 
 
 @calc_route_bp.route("/calculator/1", methods=["POST"])
-def calculator1():
+def calculator_1():
     calc = calculator1_factory()
     response = calc.calculate(request)
 
@@ -15,8 +16,16 @@ def calculator1():
 
 
 @calc_route_bp.route("/calculator/2", methods=["POST"])
-def calculator2():
+def calculator_2():
     calc = calculator2_factory()
+    response = calc.calculate(request)
+
+    return jsonify(response), 200
+
+
+@calc_route_bp.route("/calculator/3", methods=["POST"])
+def calculator_3():
+    calc = calculator3_factory()
     response = calc.calculate(request)
 
     return jsonify(response), 200
